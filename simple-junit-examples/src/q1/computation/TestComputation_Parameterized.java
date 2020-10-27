27bb0113ed9a5823dbacb2bafaec95128271b7c1
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-10-26 19:56:35 -0500

    test: add parameterized test case [q1]

--- simple-junit-examples/src/q1/computation/TestComputation_Parameterized.java
+++ simple-junit-examples/src/q1/computation/TestComputation_Parameterized.java
@@ -17,19 +17,23 @@ public class TestComputation_Parameterized {
 
 	@Parameters
 	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] { //
-				// TODO: Write Your Answer
+		return Arrays.asList(new Object[][] {
+			{ 100, 1, 20 },
+			{ 101, 2, 30 },
+			{ 102, 3, 40 }
 		});
 	}
 
 	public TestComputation_Parameterized(int input1, int input2, int expected) {
-		// TODO: Write Your Answer
+		this.mInput1 = input1;
+		this.mInput2 = input2;
+		this.mExpected = expected;
 	}
 
 	@Test
 	public void testGetDiscount() throws Exception {
-		// TODO: Write Your Answer
-		// Assert.assertEquals(mExpected, TODO: Write Your Answer);
+		int actualResult = com.getDiscount(mInput1, mInput2);
+		Assert.assertEquals(mExpected, actualResult);
 	}
 
 }
