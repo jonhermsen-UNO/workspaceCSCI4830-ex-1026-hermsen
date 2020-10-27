dee5be499fbf86cdd9c8844d123f1d944e4b7b6a
jonhermsen-UNO <jonhermsen@unomaha.edu>
2020-10-26 20:09:37 -0500

    test: add CSV file source test case [q2]

--- simple-junit-examples/src/q2/csvfilesource/TestComputation_CsvFileSource.java
+++ simple-junit-examples/src/q2/csvfilesource/TestComputation_CsvFileSource.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class TestComputation_CsvFileSource {
 
-	// TODO: Write your answer to complete this test case
 	@ParameterizedTest
-	@CsvFileSource(resources = "")
-	public void testGetDiscount() throws Exception {
-
+	@CsvFileSource(resources = "computation_dataset.csv")
+	public void testGetDiscount(int input1, int input2, int expected) throws Exception {
 		Computation com = new Computation();
-		int result = 0;
-		Assert.assertEquals(result, null);
-
+		int actualResult = com.getDiscount(input1, input2);
+		Assert.assertEquals(expected, actualResult);
 	}
 
 }
